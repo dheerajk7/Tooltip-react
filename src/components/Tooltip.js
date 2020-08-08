90669ae09c49cdbd2a6ba078b07ecc4ca8eb4f7d
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-08 22:23:37 +0530

    add tooltip position for left and right

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -5,14 +5,47 @@ class Tooltip extends Component {
   render() {
     return (
       <div className="tooltip-container">
-        <div className="button">Hover Here</div>
-        <div className="tooltip-message-container message-container-position-top ">
-          Hovered
-          <div className="arrow down-arrow"></div>
+        <div className="button">
+          Hover Here
+          <div className="tooltip-message-container message-container-position-right ">
+            Hovered
+            <div className="arrow left-arrow"></div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+const styles = {
+  downPosition: {
+    containerPosition: { top: '120%', left: '12%' },
+    arrowPosition: {
+      top: '-12px',
+      right: '20px',
+      borderBottom: '20px solid #1f0e0ecf',
+      borderLeft: '20px solid transparent',
+      borderRight: '20px solid transparent',
+    },
+  },
+  topPosition: {
+    containerPosition: { bottom: '120%', left: '12%' },
+    arrowPosition: {
+      top: '42px',
+      left: '37px',
+      borderTop: '20px solid #1f0e0ecf',
+      borderLeft: '20px solid transparent',
+      borderRight: '20px solid transparent',
+    },
+  },
+  topPosition: {
+    containerPosition: {},
+    arrowPosition: {},
+  },
+  bottomPosition: {
+    containerPosition: {},
+    arrowPosition: {},
+  },
+};
 
 export default Tooltip;
