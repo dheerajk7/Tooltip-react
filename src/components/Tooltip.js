7431ba04b191d919f3625350388b1e63623cc73e
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-08 22:37:45 +0530

    add different tooltip message container position using styles object in component

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,13 +3,18 @@ import '../assets/css/tooltip.css';
 
 class Tooltip extends Component {
   render() {
+    let style = '';
+    style = styles.leftPosition;
     return (
       <div className="tooltip-container">
         <div className="button">
           Hover Here
-          <div className="tooltip-message-container message-container-position-right ">
+          <div
+            className="tooltip-message-container"
+            style={style.containerPosition}
+          >
             Hovered
-            <div className="arrow left-arrow"></div>
+            <div className="arrow" style={style.arrowPosition}></div>
           </div>
         </div>
       </div>
@@ -38,13 +43,25 @@ const styles = {
       borderRight: '20px solid transparent',
     },
   },
-  topPosition: {
-    containerPosition: {},
-    arrowPosition: {},
+  leftPosition: {
+    containerPosition: { right: '107%', top: '16%' },
+    arrowPosition: {
+      top: '5px',
+      right: '-11px',
+      borderLeft: '20px solid #1f0e0ecf',
+      borderTop: '20px solid transparent',
+      borderBottom: '20px solid transparent',
+    },
   },
-  bottomPosition: {
-    containerPosition: {},
-    arrowPosition: {},
+  rightPosition: {
+    containerPosition: { left: '106%', top: '16%' },
+    arrowPosition: {
+      top: '5px',
+      left: '-11px',
+      borderRight: '20px solid #1f0e0ecf',
+      borderTop: '20px solid transparent',
+      borderBottom: '20px solid transparent',
+    },
   },
 };
 
