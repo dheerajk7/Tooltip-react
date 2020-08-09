1df3ba520ab3bf035f752db33f3def22604ffbe6
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-09 11:43:36 +0530

    Updates

---
+++ precache-manifest.c261f1a12a69915f19843fc59b0bd671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9d849314780988b50595b189215453",
+    "url": "/tooltip-react/index.html"
+  },
+  {
+    "revision": "407934ef13c794604a76",
+    "url": "/tooltip-react/static/css/main.2bde0cda.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/tooltip-react/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip-react/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407934ef13c794604a76",
+    "url": "/tooltip-react/static/js/main.76fcaa12.chunk.js"
+  },
+  {
+    "revision": "82e22ae5e2d77b9daca9",
+    "url": "/tooltip-react/static/js/runtime-main.f204a790.js"
+  }
+]);
